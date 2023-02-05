80acf25fce4dd817e47b01058223153c17016a70
Muhammad Arafat <97059953+Arafat05nov@users.noreply.github.com>
2023-02-05 23:11:10 +0500

    Update Error_1.py

--- Error_1.py
+++ Error_1.py
@@ -29,3 +29,6 @@ result = divide(num1, num2)
 print("Result: ", result)
 
 print("Program execution continues normally.")
+
+#The issue in the code is that we're trying to divide a number by zero, which is not allowed. 
+#To resolve the issue, you can add a conditional statement to check if b is zero before performing the division
